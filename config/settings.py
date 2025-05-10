1af781b71cd70c8d4542035c3d3a99bce3d24da2
Станислав Маяцкий <stasm226@gmail.com>
2025-05-10 16:27:39 +0500

    chore: install precommit

--- config/settings.py
+++ config/settings.py
@@ -185,4 +185,3 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 # Настройка CORS - домены, которым разрешён доступ
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
-
